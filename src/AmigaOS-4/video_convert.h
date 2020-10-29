30f5bff3dc93a83016bfa8b628b138214f8e791d
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2020-10-29 11:21:25 +0100

    Video conversion routines moved out.

---
+++ src/AmigaOS-4/video_convert.h
@@ -0,0 +1,18 @@
+
+void convert_copy_8bit( ULONG *pal, char *from, char *to,int  pixels );
+void convert_copy_16bit( ULONG *pal, char *from, char *to,int  pixels );
+void convert_copy_32bit( ULONG *pal, char *from, char *to,int  pixels );
+
+void convert_1bit_to_8bit( ULONG *pal, char *from, char *to,int  pixels );
+
+void convert_1bit_to_16bit( ULONG *pal, char *from, uint16 *to,int  pixels );
+void convert_8bit_to_16bit( ULONG *pal, char *from, uint16 *to,int  pixels );
+
+void convert_1bit_to_32bit( ULONG *pal, char *from, uint32 *to,int  pixels );
+void convert_8bit_to_32bit_db( ULONG *pal, char *from, uint32 *to,int  pixels );
+void convert_8bit_to_32bit_asm( ULONG *pal, char *from, uint32 *to,int  pixels );
+void convert_8bit_to_32bit( ULONG *pal, char *from, uint32 *to,int  pixels );
+void convert_16bit_to_32bit( ULONG *pal, uint16 *from, uint32 *to,int  pixels );
+
+
+
